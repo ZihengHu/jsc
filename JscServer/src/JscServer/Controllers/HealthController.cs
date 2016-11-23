bce84d14a82d3d287295f9ac6eb9bae4a5ceb99d
Karl Gong <karl.gong@outlook.com>
2016-11-23 10:27:02 +0800

    Add health page.

---
+++ JscServer/src/JscServer/Controllers/HealthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JscServer.Controllers
+{
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            return await Task.FromResult(Ok($"Health: OK\n{DateTime.Now.ToString("yyyy-MM-dd H:mm:ss zzz")}"));
+        }
+    }
+}
